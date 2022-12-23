a0537c140d0ec95043d1620314012a6110aa4b28
HoffsMH <matthecker@pm.me>
2022-12-23 14:36:18 -0600

    sorter draft

---
+++ sorter.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+  "fmt"
+  "io/ioutil"
+  "os"
+  "io/fs"
+)
+
+var kb int64 = 1000
+var mb int64 = 1000 * kb
+var limit int64 = 500 * mb
+
+func main() {
+  var dirs []fs.FileInfo
+
+  if len(os.Args) > 0 {
+    dirs, _ = ioutil.ReadDir(os.Args[1])
+  } else {
+    dirs, _ = ioutil.ReadDir(".")
+  }
+
+	for _, file := range dirs {
+    if file.Size() > limit {
+      fmt.Println("big boi")
+      fmt.Println(file.Size())
+      fmt.Println(file.Name())
+    }
+	}
+}
