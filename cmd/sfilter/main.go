5ef74bfce624d31f339e5e67be9f0a5c1ee796de
HoffsMH <matthecker@pm.me>
2023-04-13 15:20:08 -0500

    wiring for sfilter

--- cmd/sfilter/main.go
+++ cmd/sfilter/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 
 	util "git.mhkr.xyz/go-utils"
@@ -11,26 +12,42 @@ import (
 
 var lessThan int
 var greaterThan int
+var kb int64 = 1000
+var mb int64 = 1000 * kb
+var gb int64 = 1000 * mb
 
 var rootCmd = &cobra.Command{
 	Use: "sfilter",
 	Long: "outputs a file name to std out with a date prefix if it does not already have one",
-	Args:    cobra.MinimumNArgs(0),
+	Args:    cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		terms := util.GetTerms(args)
 
-    var kb int64 = 1000
-    var mb int64 = 1000 * kb
-    var limit int64 = 500 * mb
 
-    util.PrintList(util.SFilter(limit))
+    util.PrintList(util.SFilter(convert(terms[0], greaterThan, lessThan)))
   },
 }
 
-func convert(sizeStr string) {
+func convert(sizeStr string) int64 {
+  var num int64
   if (strings.HasSuffix(sizeStr, "G") ) {
+    num, _ = strconv.ParseInt(sizeStr[:len(sizeStr) - 1], 10, 64)
 
+    return num * gb
   }
+
+  if (strings.HasSuffix(sizeStr, "M") ) {
+    num, _ = strconv.ParseInt(sizeStr[:len(sizeStr) - 1], 10, 64)
+
+    return num * mb
+  }
+
+  if (strings.HasSuffix(sizeStr, "K") ) {
+    num, _ = strconv.ParseInt(sizeStr[:len(sizeStr) - 1], 10, 64)
+
+    return num * kb
+  }
+  return num
 }
 
 func main() {
