18980fb05939e16edb748cc0167298608469c822
HoffsMH <matthecker@pm.me>
2023-06-04 14:52:56 -0500

    ok

--- wrap.go
+++ wrap.go
@@ -50,60 +50,11 @@ func (nc *NewContent) addNewline()  {
   nc.Value.WriteRune('\n')
 }
 
-func Wrap(content string, lim int) string {
-	Debug.Println("content: ", content)
-	newContent := &NewContent{
-		Lim:      lim,
-		WordBuf:  bytes.NewBuffer([]byte{}),
-		SpaceBuf: bytes.NewBuffer([]byte{}),
-		Value:    bytes.NewBuffer([]byte{}),
-	}
-
-	for i, char := range content {
-		if char != ' ' && char != '\n' {
-			newContent.CurrentLineIndex++
-			handleNonWhitespace(i, char, content, newContent)
-			continue
-		}
-
-		if char == ' ' {
-			handleSpace(i, char, newContent)
-			continue
-		}
-
-		if char == '\n' {
-			handleNewline(i, char, content, newContent)
-			continue
-		}
-	}
-
-	return string(newContent.Value.String())
-}
-
-func handleNonWhitespace(i int, char rune, content string, nc *NewContent) {
-	wordBuf := nc.WordBuf
-	value := nc.Value
-	lim := nc.Lim
-
-	wordBuf.WriteRune(char)
-
-	if i == len(content)-1 {
-		if nc.CurrentLineIndex > lim {
-			value.WriteRune('\n')
-      nc.dumpWordBuf()
-			return
-		}
-    nc.dumpSpaceBuf()
-    nc.dumpWordBuf()
-		return
-	}
-}
-
 func (nc *NewContent) isPastOrAtLim() bool {
-	currentLineIndex := nc.CurrentLineIndex
-	lim := nc.Lim
+  currentLineIndex := nc.CurrentLineIndex
+  lim := nc.Lim
 
-	return currentLineIndex >= lim
+  return currentLineIndex >= lim
 }
 
 func (nc *NewContent) isPastLim() bool {
@@ -113,22 +64,70 @@ func (nc *NewContent) isPastLim() bool {
   return currentLineIndex > lim
 }
 
+func Wrap(content string, lim int) string {
+  Debug.Println("content: ", content)
+  newContent := &NewContent{
+    Lim:      lim,
+    WordBuf:  bytes.NewBuffer([]byte{}),
+    SpaceBuf: bytes.NewBuffer([]byte{}),
+    Value:    bytes.NewBuffer([]byte{}),
+  }
+
+  for i, char := range content {
+    if char != ' ' && char != '\n' {
+      newContent.CurrentLineIndex++
+      handleNonWhitespace(i, char, content, newContent)
+      continue
+    }
+
+    if char == ' ' {
+      handleSpace(i, char, newContent)
+      continue
+    }
+
+    if char == '\n' {
+      handleNewline(i, char, content, newContent)
+      continue
+    }
+  }
+
+  return string(newContent.Value.String())
+}
+
+func handleNonWhitespace(i int, char rune, content string, nc *NewContent) {
+  wordBuf := nc.WordBuf
+  value := nc.Value
+  lim := nc.Lim
+
+  wordBuf.WriteRune(char)
+
+  if i == len(content)-1 {
+    if nc.CurrentLineIndex > lim {
+      value.WriteRune('\n')
+      nc.dumpWordBuf()
+      return
+    }
+    nc.dumpSpaceBuf()
+    nc.dumpWordBuf()
+    return
+  }
+}
 
 func handleSpace(i int, char rune, nc *NewContent) {
-	wordBuf := nc.WordBuf
+  wordBuf := nc.WordBuf
 
-	nc.CurrentLineIndex++
-	Debug.Println("in handleSpace:")
-	Debug.Println("newContent", nc)
+  nc.CurrentLineIndex++
+  Debug.Println("in handleSpace:")
+  Debug.Println("newContent", nc)
   t := true
-	if nc.isPastOrAtLim() {
-		nc.addNewline()
+  if nc.isPastOrAtLim() {
+    nc.addNewline()
     nc.resetSpaceBuf()
-		nc.CurrentLineIndex = wordBuf.Len()
-	} else {
+    nc.CurrentLineIndex = wordBuf.Len()
+  } else {
     t = false
     nc.writeSpaceBuf()
-	}
+  }
   nc.dumpWordBuf()
   nc.resetSpaceBuf()
   if (t == false) {
@@ -137,55 +136,52 @@ func handleSpace(i int, char rune, nc *NewContent) {
 }
 
 func handleNewline(i int, char rune, content string, nc *NewContent) {
-	spaceBuf := nc.SpaceBuf
-	value := nc.Value
-	lim := nc.Lim
-	currentLineIndex := nc.CurrentLineIndex
+  currentLineIndex := nc.CurrentLineIndex
 
-	// If we have a double newline that represents a paragraph
-	// so I want to immeadialy append the 2 newlines
-	// and get ready for the new paragraph's indent level
-	if i > 0 && string(content[i-1]) == "\n" {
-		value.WriteRune('\n')
-		value.WriteRune('\n')
-		spaceBuf.Reset()
-		nc.CurrentLineIndex = 0
-		return
-	}
+  // If we have a double newline that represents a paragraph
+  // so I want to immeadialy append the 2 newlines
+  // and get ready for the new paragraph's indent level
+  if i > 0 && string(content[i-1]) == "\n" {
+    nc.addNewline()
+    nc.addNewline()
+    nc.resetSpaceBuf()
+    nc.CurrentLineIndex = 0
+    return
+  }
 
-	// we need to wrap and current char is a newline
-	if currentLineIndex > lim {
-		value.WriteRune(char)
-		nc.CurrentLineIndex = 0
+  // we need to wrap and current char is a newline
+  if nc.isPastLim() {
+    nc.addNewline()
+    nc.CurrentLineIndex = 0
     nc.dumpWordBuf()
-		if i == len(content)-1 {
-			value.WriteRune('\n')
-		}
+    if i == len(content)-1 {
+      nc.addNewline()
+    }
 
-		return
-	}
+    return
+  }
 
-	// the entire input ends in newline
-	if i == len(content)-1 {
+  // the entire input ends in newline
+  if i == len(content)-1 {
     nc.dumpSpaceBuf()
     nc.dumpWordBuf()
-		value.WriteRune(char)
-		return
-	}
+    nc.addNewline()
+    return
+  }
 
-	// the current line begins with a newline
-	if currentLineIndex == 0 {
-		value.WriteRune(char)
-		spaceBuf.Reset()
+  // the current line begins with a newline
+  if currentLineIndex == 0 {
+    nc.addNewline()
+    nc.resetSpaceBuf()
 
-		return
-	}
+    return
+  }
 
-	// if we are before the limit treat the newline as a space and write
-	if currentLineIndex < lim {
+  // if we are before the limit treat the newline as a space and write
+  if !nc.isPastOrAtLim() {
     nc.dumpSpaceBuf()
     nc.dumpWordBuf()
-		spaceBuf.WriteRune(' ')
+    nc.incSpaceBuf()
 		nc.CurrentLineIndex++
 		return
 	}
